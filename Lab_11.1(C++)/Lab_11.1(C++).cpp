1eca974d86838dd2f49255effc3abb8f248b796f
Woloda <dorozovecvoloda@gmail.com>
2020-12-14 06:35:25 +0200

    Lab_11_1_C++

---
+++ Lab_11.1(C++)/Lab_11.1(C++).cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+void operation(const char* fname, const char* gname, const int N);
+void print(const char* gname);
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char fname[101], gname[101];
+	cout << "Введіть назву фізичного файлу компонентами якого є дійсні числа: "; cin >> fname; cout << endl;
+	cout << "Введіть назву фізичного файлу компонентами, якого не можуть бути значеннями функції 'sinx': "; cin >> gname; cout << endl;
+
+	ofstream file(fname, ios::binary);
+
+	int N;
+	cout << "Введіть кількість дійсних чисел: "; cin >> N; cout << endl;
+
+	double x;
+	for (int i = 0; i < N; i++)
+	{
+		x = (200.0 - rand() % 400 * 1.0) * 0.01;
+		cout << x << endl;
+		file.write((char*)&x, sizeof(double));
+	}
+
+	file.close();
+
+	operation(fname, gname, N);
+	print(gname);
+
+	return 0;
+}
+
+void operation(const char* fname, const char* gname, const int N)
+{
+	ofstream file_s(gname, ios::binary);
+	ifstream file_l(fname, ios::binary);
+
+	int l = 0;
+	double y;
+	for (int g = 0; g < N; g++)
+	{
+		file_l.read((char*)&y, sizeof(double));
+		if (!(y <= 1 && y >= -1))
+			file_s.write((char*)&y, sizeof(double));
+	}
+
+	file_s.close();
+	file_l.close();
+}
+
+void print(const char* gname)
+{
+	ifstream file_l(gname, ios::binary);
+
+	cout << endl;
+	cout << "Компоненти даного файла '" << gname << "' які не можуть бути значеннями функції 'sinx': " << endl << endl;
+	double x;
+	double l = 4;
+	do
+	{
+		file_l.read((char*)&x, sizeof(double));
+		if (x != l)
+			cout << x << endl;
+		l = x;
+	} while (!file_l.eof());
+	file_l.close();
+}
---
+++ UnitTest(Lab_11.1(C++))/UnitTest(Lab_11.1(C++)).cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_11.1(C++)/Lab_11.1(C++).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab111C
+{
+	TEST_CLASS(UnitTestLab111C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ofstream file("E:\\Моя папка. Навчання\\Student [IK-11] [Дорожовець]\\Lab_11.1(C++)\\Lab_11.1(C++)\\test.dat", ios::binary);
+
+			double x = 0.28;
+			for (int i = 0; i < 6; i++)
+			{
+				x = x + 0.28;
+				cout << x << endl;
+				file.write((char*)&x, sizeof(double));
+			}
+			file.close();
+
+			operation("E:\\Моя папка. Навчання\\Student [IK-11] [Дорожовець]\\Lab_11.1(C++)\\Lab_11.1(C++)\\test.dat", "E:\\Моя папка. Навчання\\Student [IK-11] [Дорожовець]\\Lab_11.1(C++)\\Lab_11.1(C++)\\test1.dat", 6);
+			ifstream file_l("E:\\Моя папка. Навчання\\Student [IK-11] [Дорожовець]\\Lab_11.1(C++)\\Lab_11.1(C++)\\test1.dat", ios::binary);
+			file_l.read((char*)&x, sizeof(double));
+
+			Assert::AreEqual(x, 1.12);
+			file_l.close();
+		}
+	};
+}
